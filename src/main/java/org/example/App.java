1283f236c91419eb25f97eb0ced8bf911ea38bfd
carlosapr11 <carlospr11job@gmail.com>
2024-05-11 07:42:24 -0500

    version base

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,42 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        int edad = 19;
+        boolean bEsHombre = true;
+
+        /*
+        Solo podrán entrar a la discote los chicos mayores a 20 años,
+        y las chicas mayores a 18 años.
+         */
+        if(bEsHombre){
+            System.out.println("Es hombre");
+            System.out.println("probando GIT");
+            if(edad > 20){
+                System.out.println("Si puede entrar a la fiesta");
+            }else{
+                System.out.println("no puede entrar  a la fiesta");
+            }
+        }else{
+            System.out.println("Es Mujer");
+            if(edad > 18){
+                System.out.println("Si puede entrar a la fiesta");
+            }else{
+                System.out.println("no puede entrar  a la fiesta");
+            }
+        }
+
+
+
+
+
+
+    }
+}
